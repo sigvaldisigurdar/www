bb57be7722deca6874b1fcde93d761e74cf1ddc5
sigvaldisigurdar <97695773+sigvaldisigurdar@users.noreply.github.com>
2022-04-08 16:29:40 +0000

    Update About.vue

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="content">
     <h1 class="title is-1 has-text-centered">Um Stuðningsbankann</h1>
-    <p>Stuðningsbankinn er heimasíða á vegum Sambands íslenskra framhaldsskólanema (SÍF). Síðunni er ætlað að veita nemendum með sértæka námsörðugleika og/eða fatlanir gagnlegar upplýsingar um þjónustu og aðgengi í framhaldsskólum landssins. Einnig má finna á síðunni ýmsar upplýsingar um almenn réttindi þessara nemenda auk annars fróðleiks eins og hlekki inn á síður sem geta hjálpað við námið.</p>
-    <p>Stuðningsbankinn var unninn af hóp nemenda í MPM námi við Háskólann í Reykjavík. Hópurinn sá um upplýsingaöflun, hönnun og uppsetningu síðunnar. Einnig útbjó hópurinn og setti upp spurningalista sem sendur var á skólastjórnendur og námsráðgjafa til að fá upplýsingar um aðgengi og þjónustu í öllum framhaldsskólum landsins. </p>
-    <p>Hópurinn afhendi SÍF beta útgáfu af vefsíðunni þar sem tveir skólar höfðu svarað spurningalistanum. SÍF sá um lokaútfærslu síðunnar og sá um að fá svör frá öllum framhaldsskólum landsins til að birta á síðunni.</p>
+    <p>Stuðningsbankinn er heimasíða á vegum Sambands íslenskra framhaldsskólanema (SÍF). Síðunni er ætlað að veita nemendum með sértæka námsörðugleika og/eða fatlanir gagnlegar upplýsingar um t.d. námsúrræði, aðgengi og aðra þjónustu í framhaldsskólum landssins. Einnig má finna á síðunni ýmsar upplýsingar um almenn réttindi þessara nemenda auk annars fróðleiks eins og hlekki inn á síður sem geta hjálpað við námið.</p>
+    <p>Stuðningsbankinn var til að byrja með unninn af hóp nemenda í MPM námi við Háskólann í Reykjavík. Hópurinn sá um þarfagreiningu, frumhönnun og uppsetningu síðunnar. Einnig útbjó hópurinn og setti upp spurningalista sem sendur var á skólastjórnendur og námsráðgjafa til að fá upplýsingar um aðgengi og þjónustu í öllum framhaldsskólum landsins. </p>
+    <p>Hópurinn afhendi SÍF beta útgáfu af vefsíðunni í apríl 2021 þar sem tveir skólar höfðu svarað spurningalistanum. SÍF sá um lokaútfærslu síðunnar og sá um að fá svör frá öllum framhaldsskólum landsins til að birta á síðunni.</p>
+    </p> Allar spurningar og efni Stuðningsbankans er byggt á fjölda viðtala, notendaprófana og funda við nemendur með fatlanir og/eða námsörðugleika yfir rúmt ár. Síðan er því öll unnin með nemendum með sérþarfir fyrir nemendur með sérþarfir.</p>
+    </p> Vefstjóri síðunnar er Bergur Hallgrímsson og verkefnastjóri verkefnisins er Sigvaldi Sigurðarson </p>
   </div>
 </template>
 
