e43eefb3724ae02e0cc1566fe8a02165c116d3ff
sigvaldisigurdar <97695773+sigvaldisigurdar@users.noreply.github.com>
2022-04-08 16:36:00 +0000

    Update Welcome.vue

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="content">
     <h1 class="title is-1 has-text-centered">Velkomin í Stuðningsbanka SÍF</h1>
-    <p>Stuðningsbankinn veitir nemendum með sértæka námsörðugleika og/eða fatlanir gagnlegar upplýsingar um þjónustu og aðgengi í framhaldsskólum landssins. </p>
+    <p>Stuðningsbankinn er vefsíða á vegum Sambands íslenskra framhaldsskólanema (SÍF) sem veitir nemendum með sértæka námsörðugleika og/eða fatlanir gagnlegar upplýsingar um þjónustu og aðgengi í framhaldsskólum landssins. </p>
     <p>Einnig er að finna á síðunni ýmsar upplýsingar um réttindi þessara hópa nemenda ásamt tenglum inn á síður sem geta veitt aðstoð með námið og/eða veitt frekari upplýsingar um þau réttindi eða þá þjónustu sem er í boði. Notaðu svo leitargluggann að ofan til að kynna þér skólana betur.</p>
   </div>
 </template>
